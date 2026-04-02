f439c24b1b63f875486cacb1a73cbfa40107e918
pa1nwizzard <meeponekrisa@gmail.com>
2026-04-02 10:45:30 +0500

    add high-level module

---
+++ app.py
@@ -0,0 +1,19 @@
+def run():
+    print(">_ Lab 1: запуск модулей _<\n")
+
+    import parts._00_distance
+    import parts._01_circle
+    import parts._02_operations
+    import parts._03_favorite_movies
+    import parts._04_my_family
+    import parts._05_zoo
+    import parts._06_songs_list
+    import parts._07_secret
+    import parts._08_garden
+    import parts._09_shopping
+    import parts._10_store
+
+    print("\nВсе модули успешно выполнены -_^")
+
+if __name__ == "__main__":
+    run()
